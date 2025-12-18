577ee0944e6e4d5effce06ec3df0d7767132e1d4
MhmdRayanM7 <m7mddotm7@gmail.com>
2025-12-18 18:28:56 +0200

    feat: accept user prompt via CLI

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
-
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Gemini CLI Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
@@ -13,8 +17,8 @@ def main():
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-    )
+        contents=args.user_prompt,
+        )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
 
